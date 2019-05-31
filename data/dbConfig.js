034d0d65d5b7759a8380f7d28634c9232bcae6e0
maxgunter99 <maxgunter99@gmail.com>
2019-05-31 12:52:03 -0500

    dbConfig bug fix

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -4,8 +4,7 @@ const knex = require('knex');
 const config = require('../knexfile.js');
 
 //SETUP ⬇︎
-// const dbEnv = process.env.DB_ENV || 'development';
-const dbEnv = process.env.DB_ENV;
+const dbEnv = process.env.DB_ENV || 'development';
 
 //EXPORTS ⬇︎
 module.exports = knex(config[dbEnv]);
